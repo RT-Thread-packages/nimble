a9b47f50016bdb204d26447ecea922bc6633bd2b
Yanlineng <77085567+Yanlineng@users.noreply.github.com>
2022-08-16 19:02:55 +0800

    添加串口驱动v1、v2适配；解决keil下大小端判断不正常问题 (#22)
    * [update] 适配串口驱动v1 v2
    
    * [fix]解决 keil 工程下大小端判断问题
    
    * fix some error
    
    * adjust code

--- nimble/transport/rtthread/src/ble_hci_rtthread_uart.c
+++ nimble/transport/rtthread/src/ble_hci_rtthread_uart.c
@@ -76,12 +76,19 @@ static int rtthread_hci_uart_init(void)
     config.baud_rate = BAUD_RATE_115200;
     config.data_bits = DATA_BITS_8;
     config.stop_bits = STOP_BITS_1;
+#ifdef RT_USING_SERIAL_V2
+#else
     config.bufsz     = 128;
+#endif
     config.parity    = PARITY_NONE;
 
     rt_device_control(g_serial, RT_DEVICE_CTRL_CONFIG, &config);
 
+#ifdef RT_USING_SERIAL_V2
+    rt_device_open(g_serial, RT_DEVICE_FLAG_RX_NON_BLOCKING | RT_DEVICE_FLAG_TX_BLOCKING);
+#else
     rt_device_open(g_serial, RT_DEVICE_FLAG_INT_RX);
+#endif
 
     rt_thread_t rx_thread = rt_thread_create("hci_uart_rx", rtthread_uart_rx_entry,
                                             RT_NULL, 1024, 25, 10);
